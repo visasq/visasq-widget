2272b615d67347520cbcd6af13c575728bcbf4f3
taizawa <aizawa.tat@gmail.com>
2015-04-04 00:41:53 +0900

    add uglify, sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var src_dir = './src/**/*.js'
+var gulp = require('gulp'),
+  babel = require('gulp-babel'),
+  sourcemaps = require('gulp-sourcemaps'),
+  uglify = require('gulp-uglify'),
+  src_dir = './src/**/*.js';
+
 gulp.task('default', function () {
   return gulp.src(src_dir)
     .pipe(babel())
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dest/js'));
 });
 gulp.task('watch', function(){
